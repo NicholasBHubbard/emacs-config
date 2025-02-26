332451cff8c2248fec6a612e61bde415676aef81
Nicholas Hubbard <nicholashubbard@posteo.net>
2025-02-25 23:36:26 -0500

    more changes

--- init.el
+++ init.el
@@ -26,7 +26,7 @@
   (disabled-command-function nil)
   (warning-minimum-level :error)
   (kill-buffer-query-functions nil)
-  (revert-without-query '(""))
+  (revert-without-query '(".*"))
   (display-buffer-base-action '(display-buffer-same-window))
   :config
   (scroll-bar-mode 0)
