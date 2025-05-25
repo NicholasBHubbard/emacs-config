eae520da239148370f682bbc485280af8a75f069
Nicholas Hubbard <nicholashubbard@posteo.net>
2025-05-25 18:22:28 -0400

    test

--- init.el
+++ init.el
@@ -530,6 +530,7 @@
                                  (magit-changed-files
                                   latest-commit snd-latest-commit)))
                            (dolist (file changed-files)
+                             (message "HERE: %s" file)
                              (when-let ((buf (find-buffer-visiting file)))
                                (with-current-buffer buf
                                  (revert-buffer t t t))))))))
